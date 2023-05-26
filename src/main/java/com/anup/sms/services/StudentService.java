f4570e0e4ba35831a8b185c7f525e8dade2689a8
Anup <anup50singh50@gmail.com>
2023-05-26 10:43:54 +0530

    Initial commit

---
+++ src/main/java/com/anup/sms/services/StudentService.java
@@ -0,0 +1,19 @@
+package com.anup.sms.services;
+
+import java.util.List;
+
+import com.anup.sms.entities.Student;
+
+public interface StudentService {
+
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student) ;
+
+	Student getStudentById(Long id) ;
+	Student updateStudent(Student student);
+		
+	
+		
+	
+}
---
+++ src/main/java/com/anup/sms/services/impl/StudentServiceImpl.java
@@ -0,0 +1,57 @@
+package com.anup.sms.services.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.anup.sms.entities.Student;
+import com.anup.sms.repository.StudentRepository;
+import com.anup.sms.services.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+	private StudentRepository studentRepository ;
+	
+	
+	
+	public StudentServiceImpl(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+	
+	
+
+
+	@Override
+	public List<Student> getAllStudents() {
+		// TODO Auto-generated method stub
+		return studentRepository.findAll();
+	}
+
+
+	@Override
+	public Student saveStudent(Student student) {
+		// TODO Auto-generated method stub
+		return studentRepository.save(student);
+	}
+
+
+
+
+	@Override
+	public Student getStudentById(Long id) {
+		// TODO Auto-generated method stub
+		return studentRepository.findById(id).get();
+	}
+
+
+
+
+	@Override
+	public Student updateStudent(Student student) {
+		// TODO Auto-generated method stub
+		return studentRepository.save(student);
+	}
+	
+
+}
